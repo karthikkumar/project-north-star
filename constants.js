8666c1ae1c23cafd8ea1240f1ee9f39c51450bc9
Subrahmanya <Prasad.Subrahmanya@bain.com>
2023-03-20 13:57:01 +0100

    Updating constants

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 // constants for autocomplete  fields
-export const JobTitleOptions = ["Software Engineer", "Data Scientist"];
-export const CourseTitleOptions = ["Computer Science", "Data Science"];
+export const JobTitleOptions = ["Software Engineer", "Data Scientist", "Data Science Intern", "Graduate Assistant", "Project Engineer", "Research Assistant"];
+export const CourseTitleOptions = ["Computer Science", "Data Science", "Big Data Analytics", "Statistics"];
 export const SkillsOptions = ["Python", "Java", "C++"];
 export const UniversityOptions = [
   "University of Toronto",
-  "University of Waterloo",
+  "University of Waterloo"
 ];
 export const GeographyOptions = ["Toronto", "Waterloo"];
